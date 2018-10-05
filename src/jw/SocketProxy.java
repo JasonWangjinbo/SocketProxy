4573821fc0c5c5b98574806f67a4d986582dff21
Jason <Jason.Wangjinbo@gmail.com>
2018-10-05 23:56:36 +0900

    Initialize project

---
+++ src/jw/SocketProxy.java
@@ -0,0 +1,19 @@
+package jw;
+
+/**
+ * A proxy for socket connections, created to help test socket connections with
+ * limited bandwidth.
+ * 
+ * @author Jason
+ *
+ */
+public class SocketProxy
+{
+
+	public static void main(final String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
