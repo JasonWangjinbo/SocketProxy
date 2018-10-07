1da24f4a3f36269b4ec87a8bf29b5d80c4d3d0fe
Jason <Jason.Wangjinbo@gmail.com>
2018-10-07 23:36:58 +0900

    Add method to log exception

--- src/jw/Log.java
+++ src/jw/Log.java
@@ -14,7 +14,17 @@ public class Log
 	public static void info(final String message, final Object... parameters)
 	{
 		final String line = String.format(message, parameters);
-		final String threadName = Thread.currentThread().getName();
-		System.out.println(LocalDateTime.now().toString() + "\t" + threadName + "\t" + threadName + line);
+		System.out.println(_header() + line);
+	}
+
+	private static String _header()
+	{
+		return LocalDateTime.now().toString() + "\t" + Thread.currentThread().getName() + ":\t";
+	}
+
+	public static void exception(final Throwable e, final String message)
+	{
+		System.err.println(_header() + message);
+		e.printStackTrace();
 	}
 }
