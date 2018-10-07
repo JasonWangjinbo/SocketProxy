6fcf6a2518b4d37bf4413c32a72187f5bad9a0a8
Jason <Jason.Wangjinbo@gmail.com>
2018-10-07 17:02:40 +0900

    Add thread name to log

--- src/jw/Log.java
+++ src/jw/Log.java
@@ -14,6 +14,7 @@ public class Log
 	public static void info(final String message, final Object... parameters)
 	{
 		final String line = String.format(message, parameters);
-		System.out.println(LocalDateTime.now().toString() + " " + line);
+		final String threadName = Thread.currentThread().getName();
+		System.out.println(LocalDateTime.now().toString() + "\t" + threadName + "\t" + threadName + line);
 	}
 }
